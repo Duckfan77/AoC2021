370428c28550302ac065925b10fbed5afdb361a7
Melissa <77photon@gmail.com>
2021-12-01 00:16:17 -0600

    Add a solution for day1 using itertools

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -1,3 +1,4 @@
+use itertools::Itertools;
 use std::fs::File;
 use std::io::prelude::*;
 
@@ -8,9 +9,11 @@ fn main() {
 
     println!("Part 1:");
     part1(&text);
+    part1_iter(&text);
 
     println!("\nPart 2:");
     part2(&text);
+    part2_iter(&text);
 }
 
 fn part1(text: &str) {
@@ -38,4 +41,28 @@ fn part2(text: &str) {
             .filter(|x| x[1] > x[0])
             .count()
     );
+}
+
+fn part1_iter(text: &str) {
+    println!(
+        "{}",
+        text.lines()
+            .map(|x| x.parse::<i32>().unwrap_or(0))
+            .tuple_windows()
+            .filter(|(a, b)| b > a)
+            .count()
+    )
+}
+
+fn part2_iter(text: &str) {
+    println!(
+        "{}",
+        text.lines()
+            .map(|x| x.parse::<i32>().unwrap_or(0))
+            .tuple_windows()
+            .map(|(x, y, z)| x + y + z)
+            .tuple_windows()
+            .filter(|(a, b)| b > a)
+            .count()
+    )
 }
