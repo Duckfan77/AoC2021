95c5b25652027f578efa8c896820f527828e3aee
Melissa <77photon@gmail.com>
2021-12-01 22:43:27 -0600

    Create Day2

---
+++ day2/src/main.rs
@@ -0,0 +1,18 @@
+use std::fs::File;
+use std::io::prelude::*;
+
+fn main() {
+    let mut file: File = File::open("input").unwrap();
+    let mut text = String::new();
+    file.read_to_string(&mut text).unwrap();
+
+    println!("Part 1:");
+    part1(&text);
+
+    println!("\nPart 2:");
+    part2(&text);
+}
+
+fn part1(text: &str) {}
+
+fn part2(text: &str) {}
