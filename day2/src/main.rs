9916edc9fde25093b6a88a78bfeb16900fc8dad9
Melissa <77photon@gmail.com>
2021-12-01 23:05:22 -0600

    complete Day 2

--- day2/src/main.rs
+++ day2/src/main.rs
@@ -13,6 +13,50 @@ fn main() {
     part2(&text);
 }
 
-fn part1(text: &str) {}
+fn part1(text: &str) {
+    let mut h = 0;
+    let mut d = 0;
 
-fn part2(text: &str) {}
+    for line in text.lines() {
+        let (key, val) = line.split_once(' ').unwrap();
+        let val = val.parse::<i32>().unwrap();
+
+        match key {
+            "forward" => h += val,
+
+            "up" => d -= val,
+
+            "down" => d += val,
+
+            _ => (),
+        };
+    }
+
+    println!("{}", h * d);
+}
+
+fn part2(text: &str) {
+    let mut h = 0;
+    let mut d = 0;
+    let mut aim = 0;
+
+    for line in text.lines() {
+        let (key, val) = line.split_once(' ').unwrap();
+        let val = val.parse::<i32>().unwrap();
+
+        match key {
+            "forward" => {
+                h += val;
+                d += aim * val;
+            }
+
+            "up" => aim -= val,
+
+            "down" => aim += val,
+
+            _ => (),
+        };
+    }
+
+    println!("{}", h * d);
+}
