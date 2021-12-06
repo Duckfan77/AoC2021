90cdac47a177660bc84ccb4925ab15a5dd56d594
Melissa <77photon@gmail.com>
2021-12-05 23:35:03 -0600

    Day 6 complete

--- day6/src/main.rs
+++ day6/src/main.rs
@@ -1,3 +1,4 @@
+use std::collections::VecDeque;
 use std::fs::File;
 use std::io::prelude::*;
 
@@ -13,6 +14,36 @@ fn main() {
     part2(&text);
 }
 
-fn part1(text: &str) {}
+fn part1(text: &str) {
+    let mut fish = VecDeque::from([0, 0, 0, 0, 0, 0, 0, 0, 0]);
+    for i in text.split(',') {
+        let i = i.parse().unwrap();
+        fish[i] += 1;
+    }
 
-fn part2(text: &str) {}
+    for _ in 0..80 {
+        step(&mut fish);
+    }
+
+    println!("{}", fish.iter().map(|x| *x as i128).sum::<i128>())
+}
+
+fn part2(text: &str) {
+    let mut fish = VecDeque::from([0, 0, 0, 0, 0, 0, 0, 0, 0]);
+    for i in text.split(',') {
+        let i = i.parse().unwrap();
+        fish[i] += 1;
+    }
+
+    for _ in 0..256 {
+        step(&mut fish);
+    }
+
+    println!("{}", fish.iter().map(|x| *x as i128).sum::<i128>())
+}
+
+fn step(fish: &mut VecDeque<i64>) {
+    let new = fish.pop_front().unwrap();
+    fish[6] += new;
+    fish.push_back(new);
+}
