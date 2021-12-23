c4a2cd75a8a25a135c6e1f68220719c886aa40f1
Melissa <77photon@gmail.com>
2021-12-22 21:23:44 -0600

    day 22 is still broken

--- day22/src/main.rs
+++ day22/src/main.rs
@@ -1,3 +1,4 @@
+use euclid::*;
 use std::collections::HashSet;
 use std::fs::File;
 use std::io::prelude::*;
@@ -28,15 +29,22 @@ fn part1(text: &str) {
 }
 
 fn part2(text: &str) {
-    let mut cubes = Vec::new();
-    for (i, line) in text.lines().enumerate() {
-        //println!("{}", i);
+    let mut cubes: Vec<Box3D<i64, ()>> = Vec::new();
+    for (_i, line) in text.lines().enumerate() {
+        println!("{}", _i);
         let mut cub = Cuboid::parse_line(line);
         cub.trim(-50, 50);
-        cubes = cub.non_overlap(cubes);
+        let cuboid = Box3D::new(
+            Point3D::new(cub.p1.0, cub.p1.1, cub.p1.2),
+            Point3D::new(cub.p2.0, cub.p2.1, cub.p2.2),
+        );
+        cubes = non_overlap(&cuboid, cubes);
+        if cub.on {
+            cubes.push(cuboid);
+        }
     }
 
-    println!("{:?}\n{}", cubes, cubes.len());
+    println!("{}", cubes.len());
 
     println!("{}", cubes.iter().map(|c| c.volume()).sum::<i64>());
 }
@@ -44,8 +52,8 @@ fn part2(text: &str) {
 #[derive(Clone, Debug)]
 struct Cuboid {
     on: bool,
-    p1: (i32, i32, i32),
-    p2: (i32, i32, i32),
+    p1: (i64, i64, i64),
+    p2: (i64, i64, i64),
 }
 
 impl Cuboid {
@@ -58,7 +66,7 @@ impl Cuboid {
             .split(',')
             .flat_map(|p| p.split(".."))
             .map(|x| x.parse().unwrap())
-            .collect::<Vec<i32>>();
+            .collect::<Vec<i64>>();
 
         Cuboid {
             on,
@@ -67,142 +75,21 @@ impl Cuboid {
         }
     }
 
-    fn trim(&mut self, min: i32, max: i32) {
-        let xlower = i32::max(self.p1.0, min);
-        let xupper = i32::min(self.p2.0, max);
+    fn trim(&mut self, min: i64, max: i64) {
+        let xlower = i64::max(self.p1.0, min);
+        let xupper = i64::min(self.p2.0, max);
 
-        let ylower = i32::max(self.p1.1, min);
-        let yupper = i32::min(self.p2.1, max);
+        let ylower = i64::max(self.p1.1, min);
+        let yupper = i64::min(self.p2.1, max);
 
-        let zlower = i32::max(self.p1.2, min);
-        let zupper = i32::min(self.p2.2, max);
+        let zlower = i64::max(self.p1.2, min);
+        let zupper = i64::min(self.p2.2, max);
         self.p1 = (xlower, ylower, zlower);
         self.p2 = (xupper, yupper, zupper);
     }
-
-    fn splitx(self, x: i32) -> Vec<Self> {
-        let mut out = Vec::new();
-
-        if self.p1.0 < x && self.p2.0 > x {
-            out.push(Self {
-                on: self.on,
-                p1: (self.p1.0, self.p1.1, self.p1.2),
-                p2: (x, self.p2.1, self.p1.2),
-            });
-            out.push(Self {
-                on: self.on,
-                p1: (x, self.p1.1, self.p1.2),
-                p2: (self.p2.0, self.p2.1, self.p2.2),
-            });
-        } else {
-            out.push(self)
-        }
-
-        //println!("x: {}", out.len());
-
-        out
-    }
-
-    fn splity(self, y: i32) -> Vec<Self> {
-        let mut out = Vec::new();
-
-        if self.p1.1 < y && self.p2.1 > y {
-            out.push(Self {
-                on: self.on,
-                p1: (self.p1.0, self.p1.1, self.p1.2),
-                p2: (self.p2.0, y, self.p1.2),
-            });
-            out.push(Self {
-                on: self.on,
-                p1: (self.p1.0, y, self.p1.2),
-                p2: (self.p2.0, self.p2.1, self.p2.2),
-            });
-        } else {
-            out.push(self)
-        }
-
-        //println!("y: {}", out.len());
-
-        out
-    }
-
-    fn splitz(self, z: i32) -> Vec<Self> {
-        let mut out = Vec::new();
-
-        if self.p1.2 < z && self.p2.2 > z {
-            out.push(Self {
-                on: self.on,
-                p1: (self.p1.0, self.p1.1, self.p1.2),
-                p2: (self.p2.0, self.p2.1, z),
-            });
-            out.push(Self {
-                on: self.on,
-                p1: (self.p1.0, self.p1.1, z),
-                p2: (self.p2.0, self.p2.1, self.p2.2),
-            });
-        } else {
-            out.push(self)
-        }
-
-        //println!("z: {}", out.len());
-
-        out
-    }
-
-    fn intersect(&self, other: &Self) -> bool {
-        Self::range_intersect((self.p1.0, self.p2.0), (other.p1.0, other.p2.0))
-            && Self::range_intersect((self.p1.1, self.p2.1), (other.p1.1, other.p2.1))
-            && Self::range_intersect((self.p1.2, self.p2.2), (other.p1.2, other.p2.2))
-    }
-
-    fn range_intersect(r1: (i32, i32), r2: (i32, i32)) -> bool {
-        let range1 = r1.0..=r1.1;
-        let range2 = r2.0..=r2.1;
-
-        range1.contains(&r2.0)
-            || range1.contains(&r2.1)
-            || range2.contains(&r1.0)
-            || range2.contains(&r1.1)
-    }
-
-    fn volume(&self) -> i64 {
-        (self.p2.0 - self.p1.0) as i64
-            * (self.p2.1 - self.p1.1) as i64
-            * (self.p2.2 - self.p1.2) as i64
-    }
-
-    fn non_overlap(self, mut others: Vec<Self>) -> Vec<Self> {
-        let mut new = Vec::new();
-
-        //make new contain the cuboids from others, split so they don't intersect self
-        for cub in others.drain(..) {
-            if cub.intersect(&self) {
-                let split: Vec<Self> = [cub]
-                    .into_iter()
-                    .flat_map(|c| c.splitx(self.p1.0).into_iter())
-                    .flat_map(|c| c.splitx(self.p2.0).into_iter())
-                    .flat_map(|c| c.splity(self.p1.1).into_iter())
-                    .flat_map(|c| c.splity(self.p2.1).into_iter())
-                    .flat_map(|c| c.splitz(self.p1.2).into_iter())
-                    .flat_map(|c| c.splitz(self.p2.2).into_iter())
-                    .collect();
-
-                new.extend(split.into_iter()); //.filter(|c| !c.intersect(&self)));
-            } else {
-                new.push(cub);
-            }
-        }
-
-        //if the new cuboid should be in the set, add it
-        if self.on {
-            new.push(self);
-        }
-
-        new
-    }
 }
 
-fn apply_cuboid(cub: &Cuboid, map: &mut HashSet<(i32, i32, i32)>) {
+fn apply_cuboid(cub: &Cuboid, map: &mut HashSet<(i64, i64, i64)>) {
     for x in cub.p1.0..=cub.p2.0 {
         for y in cub.p1.1..=cub.p1.1 {
             for z in cub.p1.2..=cub.p2.2 {
@@ -214,4 +101,94 @@ fn apply_cuboid(cub: &Cuboid, map: &mut HashSet<(i32, i32, i32)>) {
             }
         }
     }
+}
+
+fn split_cub(cub: &Box3D<i64, ()>, splitee: &Box3D<i64, ()>) -> Vec<Box3D<i64, ()>> {
+    let mut out = Vec::new();
+
+    let intersect = match cub.intersection(&splitee) {
+        None => {
+            // no intersection, no splitting to do
+            out.push(*splitee);
+            return out;
+        }
+        Some(i) => i,
+    };
+
+    let mut xrange = [Option::<(i64, i64)>::None; 3];
+    let mut yrange = [Option::<(i64, i64)>::None; 3];
+    let mut zrange = [Option::<(i64, i64)>::None; 3];
+
+    //x
+    let min = i64::min(cub.min.x, splitee.min.x);
+    let max = i64::max(cub.max.x, splitee.max.x);
+    if min != intersect.min.x {
+        xrange[0] = Some((min, intersect.min.x));
+    }
+    xrange[1] = Some((intersect.min.x, intersect.max.x));
+    if max != intersect.max.x {
+        xrange[2] = Some((intersect.max.x, max));
+    }
+
+    //y
+    let min = i64::min(cub.min.y, splitee.min.y);
+    let max = i64::max(cub.max.y, splitee.max.y);
+    if min != intersect.min.y {
+        yrange[0] = Some((min, intersect.min.y));
+    }
+    yrange[1] = Some((intersect.min.y, intersect.max.y));
+    if max != intersect.max.y {
+        yrange[2] = Some((intersect.max.y, max));
+    }
+
+    //z
+    let min = i64::min(cub.min.z, splitee.min.z);
+    let max = i64::max(cub.max.z, splitee.max.z);
+    if min != intersect.min.z {
+        zrange[0] = Some((min, intersect.min.z));
+    }
+    zrange[1] = Some((intersect.min.z, intersect.max.z));
+    if max != intersect.max.z {
+        zrange[2] = Some((intersect.max.z, max));
+    }
+
+    for x in xrange {
+        let x = match x {
+            None => continue,
+            Some(i) => i,
+        };
+        for y in yrange {
+            let y = match y {
+                None => continue,
+                Some(i) => i,
+            };
+            for z in zrange {
+                let z = match z {
+                    None => continue,
+                    Some(i) => i,
+                };
+                out.push(Box3D::new(
+                    Point3D::new(x.0, y.0, z.0),
+                    Point3D::new(x.1, y.1, z.1),
+                ))
+            }
+        }
+    }
+
+    out
+}
+
+fn non_overlap(cub: &Box3D<i64, ()>, mut boxes: Vec<Box3D<i64, ()>>) -> Vec<Box3D<i64, ()>> {
+    let mut out = Vec::new();
+
+    for other in boxes.drain(..) {
+        if cub.intersects(&other) {
+            let split = split_cub(cub, &other);
+            out.extend(split.into_iter().filter(|c| !c.intersects(cub)));
+        } else {
+            out.push(other);
+        }
+    }
+
+    out
 }
